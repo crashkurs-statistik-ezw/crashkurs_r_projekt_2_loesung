f106988926fe2446d35a6e24a7a27496200801f2
Christian <burkhart.christian@gmail.com>
2022-08-24 13:35:49 +0200

    feat: feedback marie project 2

--- analysis/descriptive_analyse.R
+++ analysis/descriptive_analyse.R
@@ -5,132 +5,139 @@ library(haven)
 
 # 1.0 Daten einlesen ------------------------------------------------------
 
+# 1.0.0
 # * Lese den Datensatz data/student_data.csv ein
 # * Speichere den Datensatz in der Variable student_data
-student_data <- read_csv(paste0(
-  "~/Hiwijob 2022/project_1_crashkurs_r/data/student_data.csv"
-))
+student_data <- read_csv("data/student_data.csv")
 
 
 # 1.1 Daten bereinigen ---------------------------------------------
 
-# * Reinige die Variablen mit der Funktion clean_names()
-# * Kodiere die Variable Pstatus um: T == together, A == apart
-# * Behalte dabei den Variablennamen
-# * Speichere den bereinigten Datensatz als student_data_cleaned
+# 1.1.0
+# * Wandle die Variablennamen mit clean_names in snake case um
+# * Kodiere die Variable pstatus mit case_when um: T -> together, A -> apart
+#   Speichere die umkodierte Variable unter dem gleichen Variablennamen pstatus
+# * Verbinde beide Bereinigungsschritte mit dem Pipe-Operator
+# * Berechne den Mittelwert der Mathenote unter der neuen Variable mean_grade_math
+# * Speichere den bereinigten Datensatz in der Variable student_data_cleaned
 student_data_cleaned <- student_data %>% 
-  clean_names() %>%
+  clean_names(case = "snake") %>%
   mutate(
     pstatus = case_when(
       pstatus == "T" ~ "together",
       pstatus == "A" ~ "apart"
     )
+  ) %>%
+  mutate(
+    mean_grade_math = (g1 + g2 + g3) / 3
   )
 
-# @Christian: wenn wir clean_names und mutate verbinden, muss ich pstatus
-# kleinschreiben, obwohl es in der Originalversion groß ist. Das müsste man
-# den Studierenden erklären oder die Funktionen doch nacheinander ausführen.
 
+# 1.2 Daten explorieren  ------------------------------------------------
 
-# 1.2 Daten transformieren ------------------------------------------------
-
-# * Wie viele Männer und Frauen sind im Datensatz?
+# 1.2.0
+# * Wie viele Maenner und Frauen sind im Datensatz?
 student_data_cleaned %>%
-  count(sex, na.rm = TRUE)
+  count(sex)
 
-# * Bestimme die Spannweite und den Mittelwert des Alters aller Probanden
+# 1.2.1
+# * Bestimme die Spannweite und den Mittelwert des Alters aller Proband*innen
 range(student_data_cleaned$age)
 mean(student_data_cleaned$age, na.rm = TRUE)
 
-# * Wie viele Probanden waren in Familien mit mehr als drei und wieviele mit
-#   weniger/gleich drei Familienmitgliedern?
+# 1.2.2
+# * Wie viele Proband*innen leben in Familien mit mehr als drei und wieviele mit
+#   weniger oder gleich drei Familienmitgliedern?
 student_data_cleaned %>%
   count(famsize)
 
-# * Vergleiche, inwiefern sich Probanden in der Qualität ihrer familiären 
-#   Bindungen unterscheiden, wenn ihre Eltern zusammen oder getrennt leben.
-#   Berechne dazu den Mittelwert für beide Gruppe mit den Funktionen group_by
-#   und summarise.
+# 1.2.3
+# Vergleiche, inwieweit sich die Probanden in der Qualität ihrer familiären 
+# Bindungen unterscheiden, wenn ihre Eltern zusammen oder getrennt leben.  
+# Berechne den Mittelwert für beide Gruppen der Variable pstatus mit 
+# den Funktionen group_by und fassen Sie zusammen.
 student_data_cleaned %>%
   group_by(pstatus) %>%
   summarise(
     mean_famrel = mean(famrel)
   )
 
-# * Untersuche auch, wie sich Probanden aus kleinen und größeren Familien in 
-#   ihrer durchschnittlichen Mathenote unterscheiden. Erstelle dafür zunächst
-#   eine neue Variable mit Hilfe von mutate und speichere den Output ab.
-# * Haben Schüler*innen aus kleinen Familien bessere Noten?
-
-# TODO: Prüfen, ob Ergebnisse stimmen. @ Christian: ist geprüft,
-# stimmen auch ohne rowwise 
-student_data_cleaned <- student_data_cleaned %>%
-  mutate(
-    mean_grade = (g1 + g2 + g3) / 3
-  )
-
+# 1.2.4
+# Untersuche, wie sich Probanden aus kleinen und größeren Familien in 
+# ihrer durchschnittlichen Mathenote unterscheiden. 
+# Haben Schüler*innen aus kleinen Familien bessere Noten?
 student_data_cleaned %>%
   group_by(famsize) %>%
   summarise(
-    mean_grade = mean(mean_grade)
+    mean_grade_math = mean(mean_grade_math)
   )
 
 
 # 1.3 Daten visualisieren -----------------------------------------------------
 
-# Untersuche den Datensatz mit Hilfe von Visualisierungen noch genauer.
+# 1.3.0
+# Untersuche den Datensatz mit Hilfe eines Balkendiagramms
 # * Erstelle ein aneinandergereihtes Balkendiagramm, das die Verteilung der 
-#   Probanden auf die verschiedenen Level von familiärer Bindungsqualität zeigt 
-#   und zusätzlich das Geschlecht beachtet.
-# * Nutze position = "dodge", um die Balken nebeneinander zu reihen.
-# * Füge einen sinnvollen Titel, Achsen- und Legendentitel ein.
+#   Probanden auf die verschiedenen Level von familiärer Bindungsqualität d
+#   auf der X-Achse darstellt und das Geschlecht durch die Farbe der 
+#   Balken kennzeichnet
+# * Nutze position = position_dodge(), um die Balken nebeneinander zu reihen.
+# * Füge einen sinnvolle Achsen- und Legendentitel hinzu
 # * Haben die männlichen Probanden bessere familiäre Bindungen als die 
 #   weiblichen?
 ggplot(student_data_cleaned, aes(x = famrel, fill = sex)) +
-  geom_bar(position = "dodge") +
+  geom_bar(position = position_dodge()) +
   labs(
-    title = "Qualität der familiären Bindung",
     x     = "Qualität der familiären Bindung",
     y     = "Anzahl",
     fill  = "Geschlecht"
-  )
+  ) +
+  scale_y_continuous(expand = expansion(0)) +
+  scale_fill_viridis_d(option = "cividis")
 
-# * Erstelle ein weiteres aneinandergereihtes Balkendiagramm mit dem 
-#   Bildungslevel der Mütter auf der x-Achse und deren Beschäftigungsstatus.
-# * Erstelle dafür eine neue bedingte Variable mit Hilfe von case_when.
+
+# 1.3.1
+# * Erstelle ein aneinandergereihtes Balkendiagramm mit dem 
+#   Bildungslevel der Mütter auf der x-Achse und deren Beschäftigungsstatus
+#   auf der Y-Achse.
+# * Erstelle  eine neue bedingte Variable mit Hilfe von case_when.
 # * Unterscheiden sich die arbeitenden Mütter von den nicht arbeitenden Mütter
 #   in ihrem Bildungslevel?
-student_data_cleaned <- student_data_cleaned %>%
+student_data_cleaned %>%
   mutate(
     working_mother = case_when(
-      mjob == "at_home" ~ "no",
-      mjob == "health" ~ "yes",
-      mjob == "other" ~ "yes",
-      mjob == "services" ~ "yes",
-      mjob == "teacher" ~ "yes"
+      mjob %in% c("at_home") ~ "no",
+      TRUE ~ "yes"
     )
-  )
-
-ggplot(student_data_cleaned, aes(x = medu, fill = working_mother)) +
+  ) %>%  
+  ggplot(aes(x = medu, fill = working_mother)) +
   geom_bar(position = "dodge") +
   labs(
-    title = "Bildungslevel Mutter",
-    x     = "Bildungslevel",
+    x     = "Bildungslevel der Mutter",
     y     = "Anzahl",
     fill  = "Working mother"
-  )
+  ) +
+  scale_y_continuous(expand = expansion(0)) 
 
-# * Speichere beide Visualisierungen über die grafische Nutzeroberfläche
-#   im R-Projekt ab. Gib den Dateien dabei sinnvolle Namen.
 
-# @Christian: Daten exportieren finde ich in keinem der Punkte aus R for Data
-# Science wieder, vielleicht hast du eine bessere Idee zur Strukturierung
+# 1.3.2
+# Speichere beide Visualisierungen im R-Projekt ab unter dem Pfad
+# images/barbplot_mothers_education_status.png
+ggsave("images/barbplot_mothers_education_status.png",
+       dpi = 300)
 
-# * Exportiere den Datensatz in den Ordner data/cleaned und
-# * Benenne die Daten als student_data_cleaned.csv
-write_csv(student_data_cleaned, "student_data_cleaned.csv")
 
-# * Um die Daten in SPSS zu nutzen, exportiere den gereinigten Datensatz mit der
-#   Funktion write_sav unter dem Namen student_data_cleaned.sav in den Ordner 
-#   data/export
-write_sav(student_data_cleaned, "student_data_cleaned.sav")
+
+# 1.4 Datenexport ---------------------------------------------------------
+
+# 1.4.0
+# * Exportiere den Datensatz in den Ordner data/cleaned
+# * Speichere die Daten unter data/export/student_data_cleaned.csv
+write_csv(student_data_cleaned, "data/export/student_data_cleaned.csv")
+
+
+# 1.4.1
+# Um die Daten in SPSS zu nutzen, exportiere den gereinigten Datensatz mit der
+# Funktion write_sav
+# Speichere die Daten unter data/export/student_data_cleaned.sav
+write_sav(student_data_cleaned, "data/export/student_data_cleaned.sav")
