a9f76e549eb28e1107632cf46464bb0bbacfb7e2
marievreden <vredenmarie@gmail.com>
2022-09-14 20:01:16 +0200

    ressourcen eingefügt

--- analysis/datenexploration.R
+++ analysis/datenexploration.R
@@ -1,22 +1,22 @@
 # 2.5 Daten explorieren  ------------------------------------------------
 
-# 2.5.1
+# 2.5.1 Geschlechterverteilung ansehen
 # * Wie viele Maenner und Frauen sind im Datensatz?
 student_data_cleaned %>%
   count(sex)
 
-# 2.5.2
+# 2.5.2 Mittelwert und Spannweite des Alters
 # * Bestimme die Spannweite und den Mittelwert des Alters aller SuS
 range(student_data_cleaned$age)
 mean(student_data_cleaned$age, na.rm = TRUE)
 
-# 2.5.3
+# 2.5.3 Familiengröße der SuS
 # * Wie viele SuS leben in Familien mit mehr als drei und wieviele mit
 #   weniger oder gleich drei Familienmitgliedern?
 student_data_cleaned %>%
   count(famsize)
 
-# 2.5.4
+# 2.5.4 Zusammenhang Familiäre Bindungen und Beziehungsstatus Eltern
 # * Vergleiche, inwieweit sich die SuS in der Qualitaet ihrer familiaeren 
 #   Bindungen unterscheiden, wenn ihre Eltern zusammen oder getrennt leben.  
 # * Berechne den Mittelwert für beide Gruppen der Variable pstatus mit 
@@ -27,7 +27,7 @@ student_data_cleaned %>%
     mean_famrel = mean(famrel)
   )
 
-# 2.5.5
+# 2.5.5 Zusammenhang Noten und Familiengröße
 # * Untersuche, wie sich SuS aus kleinen und groeßeren Familien in 
 #   ihrer durchschnittlichen Mathenote unterscheiden. 
 # * Haben SuS aus kleinen Familien bessere Noten?
@@ -40,15 +40,16 @@ student_data_cleaned %>%
 
 # 2.6 Daten visualisieren -----------------------------------------------------
 
-# 2.6.1
+# 2.6.1 Balkendiagramm  zu familiären Bindungen und Geschlecht
 # Untersuche den Datensatz mit Hilfe eines Balkendiagramms
 # * Erstelle ein aneinandergereihtes Balkendiagramm, das die Verteilung der 
-#   SuS auf die verschiedenen Level von familiaerer Bindungsqualitaet d
-#   auf der X-Achse darstellt und das Geschlecht durch die Farbe der 
-#   Balken kennzeichnet
+#   SuS auf die verschiedenen Level von familiaerer Bindungsqualitaet auf der 
+#   X-Achse darstellt und das Geschlecht durch die Farbe der Balken kennzeichnet
 # * Nutze position = position_dodge(), um die Balken nebeneinander zu reihen.
 # * Füge sinnvolle Achsen- und Legendentitel hinzu
 # * Haben die Schueler bessere familiaere Bindungen als Schuelerinnen?
+# * Brauchst du Hilfe?
+#   https://statdoe.com/barplot-for-two-factors-in-r/#using-colour-to-split-the-results
 ggplot(student_data_cleaned, aes(x = famrel, fill = sex)) +
   geom_bar(position = position_dodge()) +
   labs(
@@ -60,16 +61,16 @@ ggplot(student_data_cleaned, aes(x = famrel, fill = sex)) +
   scale_fill_viridis_d(option = "cividis", begin = 0.3, end = 0.9)
 
 
-# 2.6.2
+# 2.6.2 Visualisierung speichern
 # Speichere die Visualisierung im R-Projekt ab unter dem Pfad
 # images/barbplot_mothers_education_status.png
 ggsave("images/verteilung_bildungsqualitaet_geschlecht.png",
        width = 8, height = 5, dpi = 300)
 
 
-# 2.6.3
+# 2.6.3 Balkendiagramm Bildungslevel Mütter
 # * Erstelle ein aneinandergereihtes Balkendiagramm mit dem Bildungslevel der 
-#   Muetter auf der x-Achse und deren Beschaeftigungsstatus auf der Y-Achse.
+#   Muetter auf der X-Achse und deren Beschaeftigungsstatus auf der Y-Achse.
 # * Erstelle  eine neue bedingte Variable mit Hilfe von case_when.
 # * Unterscheiden sich die arbeitenden Muetter von den nicht arbeitenden 
 #   Muettern in ihrem Bildungslevel?
@@ -91,7 +92,7 @@ student_data_cleaned %>%
   scale_fill_viridis_d(option = "cividis", begin = 0.3, end = 0.9)
 
 
-# 2.6.4
+# 2.6.4 Visualisierung speichern
 # Speichere die Visualisierung im R-Projekt ab unter dem Pfad
 # images/barbplot_mothers_education_status.png
 ggsave("images/barbplot_mothers_education_status.png", width = 8,
